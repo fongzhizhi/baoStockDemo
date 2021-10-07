af5c0db8e10a50bf67adc1eb90c100e791ddeeae
fongzhizhi <1837486996@qq.com>
2021-10-07 23:35:18 +0800

    更新api

--- app.py
+++ app.py
@@ -9,11 +9,19 @@ def init():
 init()
 app = Flask(__name__)
 
-@app.route('/get_k_data_json')
+@app.route('/get_k_data_json', methods=["POST"])
 def user():
     data = request.get_json()
-    code = data['code']
+    codes = None
+    # 多股
+    if 'codes' in data:
+        codes = data['codes']
+    # 单股
+    else:
+        codes = [data['code']]
     start = data['start']
     end = data['end']
-    res = get_k_data_json(code, start, end)
+    res = {}
+    for code in codes:
+        res[code] = get_k_data_json(code, start, end)
     return res
